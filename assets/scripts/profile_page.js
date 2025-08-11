ab459f0e55f3baa5e7741c0879c613b428709dc7
isaacszn <isaaczik9@gmail.com>
2025-08-11 16:37:17 +0100

    update profile_page.js

--- assets/scripts/profile_page.js
+++ assets/scripts/profile_page.js
@@ -25,34 +25,37 @@ document.querySelector('#image-upload').addEventListener('change', () => {
   // Piece of code to process image user has selected, get its src and save it
   // to localStorage
   const file = document.querySelector('#image-upload').files[0];
-  console.log(file)
   if (file) {
-    const fileSrc = URL.createObjectURL(file);
-    document.querySelector('#profile-pfp').src = fileSrc;
-    // Save file src to LocalStorage
-    let userData = localStorage.getItem('userdata');
-    userData = JSON.parse(userData);
-    const fullName = userData.fullname.trim();
-    const userName = userData.username.trim();
-    const email = userData.email.trim();
-    const password = userData.password.trim();
-    const confirmPassword = userData.confirmpassword.trim();
-    const balance = userData.balance;
-    const recentTransaction = userData.recent;
-    const pfpSrc = userData.pfp.trim();
-    const userDataNew = {
-      fullname: fullName,
-      username: userName,
-      email: email,
-      password: password,
-      confirmpassword: confirmPassword,
-      balance: balance,
-      recent: recentTransaction,
-      pfp: pfpSrc
+    const reader = new FileReader();
+    reader.onload = () => {
+      const fileSrc = reader.result;
+      document.querySelector('#profile-pfp').src = fileSrc;
+      // Save file src to LocalStorage
+      let userData = localStorage.getItem('userdata');
+      userData = JSON.parse(userData);
+      const fullName = userData.fullname.trim();
+      const userName = userData.username.trim();
+      const email = userData.email.trim();
+      const password = userData.password.trim();
+      const confirmPassword = userData.confirmpassword.trim();
+      const balance = userData.balance;
+      const recentTransaction = userData.recent;
+      const pfpSrc = userData.pfp.trim();
+      const userDataNew = {
+        fullname: fullName,
+        username: userName,
+        email: email,
+        password: password,
+        confirmpassword: confirmPassword,
+        balance: balance,
+        recent: recentTransaction,
+        pfp: pfpSrc
+      };
+      userDataNew.pfp = fileSrc;
+      localStorage.removeItem('userdata');
+      localStorage.setItem('userdata', JSON.stringify(userDataNew));
     };
-    userDataNew.pfp = fileSrc;
-    localStorage.removeItem('userdata');
-    localStorage.setItem('userdata', JSON.stringify(userDataNew));
+    reader.readAsDataURL(file);
   }
 });
 
